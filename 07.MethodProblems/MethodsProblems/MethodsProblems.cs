a8976982b627ef4f8c239d1e5b25a4427407afb6
KaloianK <olpenguin30@gmail.com>
2021-03-29 18:41:05 +0300

    Methods Commit

---
+++ 07.MethodProblems/MethodsProblems/MethodsProblems.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace MethodsProblems
+{
+    class MethodsProblems
+    {
+        /*1
+          static string PrintName(string name)
+        {
+            Console.Write("Hello, {0}!", name);
+            return name;
+        }*/
+
+        /* 2
+          static int GetMax(int firstNumber, int secondNumber)
+          {
+              return firstNumber > secondNumber ? firstNumber : secondNumber;
+
+          }
+          static int MaxOfThree(int biggerNumber, int thirdNumber)
+          {
+              return biggerNumber > thirdNumber ? biggerNumber : thirdNumber;
+          }*/
+      
+
+
+        static void Main(string[] args)
+        {
+            //1 PrintName("Kaloian");
+
+            /*2 
+              Console.Write("Enter three numbers: ");
+              string number = Console.ReadLine();
+              string[] splitNumbers = number.Split(' ');
+              int firstNumber;
+              int.TryParse(splitNumbers[0], out firstNumber);
+              int secondNumber;
+              int.TryParse(splitNumbers[1], out secondNumber);
+              int thirdNumber;
+              int.TryParse(splitNumbers[2], out thirdNumber);
+              Console.WriteLine("The biggest number of them all is: " + MaxOfThree(GetMax(firstNumber, secondNumber), thirdNumber));*/
+
+           
+
+
+        }
+    }
+}
---
+++ 07.MethodProblems/StringifyNumbers/StringifyNumbers.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+namespace StringifyNumbers
+{
+    class StringifyNumbers
+    {
+        static string GetSingular(int singular)
+        {
+            string[] singularArray = new string[] { "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine" };
+
+            return singularArray[singular];
+        }
+
+        static string GetDecimals(int decimals, int singulars)
+        {
+            string result;
+            if (decimals == 1)
+            {
+                switch (decimals + singulars)
+                {
+                    case 1: result = "Ten"; break;
+                    case 2: result = "Eleven"; break;
+                    case 3: result = "Twelve"; break;
+                    case 4: result = "Thirteen"; break;
+                    case 6: result = "Fifteen"; break;
+                    case 9: result = "Eighteen"; break;
+                    default: result = GetSingular(singulars) + "teen"; break;
+                }
+            }
+            else
+            {
+                switch (decimals)
+                {
+                    case 0: result = ""; break;
+                    case 2: result = "Twenty"; break;
+                    case 3: result = "Thirty"; break;
+                    case 5: result = "Fifty"; break;
+                    case 8: result = "Eighty"; break;
+                    default: result = GetSingular(decimals) + "ty"; break;
+                }
+            }
+
+
+            return result;
+        }
+
+        static string GetHundreds(int hundreds)
+        {
+            return hundreds != 0 ? GetSingular(hundreds) + "-hundred" : "";
+        }
+
+        static void Main(string[] args)
+        {
+
+
+            Console.Write("Enter a number between 1 and 1000: ");
+            string input = Console.ReadLine();
+            int numbers = int.Parse(input);
+            int[] numArray = new int[4];
+            int lastDigit;
+            int number = numbers;
+
+            for (int i = 0; i < 4; i++)
+            {
+                lastDigit = numbers % 10;
+                numArray[i] = lastDigit;
+                numbers /= 10;
+            }
+            int singulars = numArray[0];
+            int decimals = numArray[1];
+            int hundreds = numArray[2];
+            string result = "";
+
+            if (number == 0)
+            {
+                result = GetSingular(singulars);
+            }
+            else if (number == 1000)
+            {
+                result = "One Thousand!";
+            }
+            else if (number > 1000)
+            {
+                result = "Number out of reach!  :(";
+            }         
+            else
+            {
+                if (hundreds != 0)
+                {
+                    result = GetHundreds(hundreds);
+                }
+
+                if (decimals != 0)
+                {
+                    if (hundreds != 0)
+                    {
+                        result += " and ";
+                    }
+
+                    result += GetDecimals(decimals, singulars);
+                }
+
+                if (decimals != 1 && singulars != 0)
+                {
+                    if (hundreds != 0 && decimals == 0)
+                    {
+                        result += " and ";
+                    }
+
+                    result += GetSingular(singulars);
+                }
+            }
+
+            Console.Write(result);
+        }
+    }
+}
