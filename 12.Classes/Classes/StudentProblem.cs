ee16f37fef567780f3e1694380aa5914616cf4bd
KaloianK <olpenguin30@gmail.com>
2021-04-14 11:49:25 +0300

    Student Problem improved with some thrown exceptions

--- 12.Classes/Classes/StudentProblem.cs
+++ 12.Classes/Classes/StudentProblem.cs
@@ -6,10 +6,8 @@ namespace Classes
 {
     class StudentProblem
     {
-        private string threeNames;
-        //full name
-        //first name
-        // last name
+        private string firstName;
+        private string lastName;
         private int year;
         private string course;
         private string university;
@@ -17,23 +15,72 @@ namespace Classes
         private int phoneNumber;
         public static int counter = 0;
 
-        public StudentProblem(string threeNames, int year, string course, string university, string mail, int phoneNumber)
+        public StudentProblem(string firstName, string lastName, int year, string course, string university, string mail, int phoneNumber)
         {
-            this.threeNames = threeNames;           
+
             this.year = year;
             this.course = course;
             this.university = university;
             this.mail = mail;
             this.phoneNumber = phoneNumber;
+            this.firstName = firstName;
+            this.lastName = lastName;
+
+          
             counter++;
         }
-        //public string fullNames
-        //{
-        //    get
-        //    {
-        //        return firstName + " " + lastName
-        //    }
-        //}
+
+
+
+        public StudentProblem(string firstName, string lastName)
+        {
+
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return this.firstName;
+            }
+            set
+            {
+                firstName = value;
+                if (firstName == "")
+                {
+                    throw new ArgumentNullException("You didn't enter your First Name. Please try again!");
+
+                }
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return this.lastName;
+            }
+            set
+            {
+                lastName = value;
+
+                if (lastName == "")
+                {
+                    throw new ArgumentNullException("You didn't enter your Last Name. Please try again!");
+
+                }
+            }
+        }
+
+        public string FullName
+        {
+            get
+            {
+                return firstName + " " + lastName;
+            }
+
+        }
+
         public int Counter
         {
             get
@@ -47,14 +94,79 @@ namespace Classes
             }
         }
 
+        public string Course
+        {
+            get
+            {
+                return this.course;
+            }
+            set
+            {
+                course = value;
+            }
+        }
+
+        public string University
+        {
+            get
+            {
+                return this.university;
+            }
+            set
+            {
+                university = value;
+                if (university == "")
+                {
+                    throw new ArgumentNullException("You didn't enter your University. Please try again!");
+
+                }
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return this.mail;
+            }
+            set
+            {
+                mail = value;
+            }
+        }
+
+        public int Year
+        {
+            get
+            {
+                return this.year;
+            }
+            set
+            {
+                year = value;
+            }
+        }
+
+        public int PhoneNumber
+        {
+            get
+            {
+                return this.phoneNumber;
+            }
+            set
+            {
+                phoneNumber = value;
+            }
+        }
+
         public void PrintStudentInfo()
         {
-            Console.WriteLine("Student's three names are: " + this.threeNames + ". He is year" + this.year + ". His course is called: " + this.course + (" in the " + this.university) +
-                 ". His mail is: " + this.mail + " and has Phone Number like this: 0" + this.phoneNumber);
+            Console.WriteLine("Student's names are: " + this.FullName + ". He is year " + this.Year + ". His course is called: " + this.Course + (" in the " + this.University) +
+                 ". His mail is: " + this.Email + " and has Phone Number like this: 0" + this.PhoneNumber);
             Console.WriteLine(counter);
         }
 
-       
+
 
     }
 }
--- 12.Classes/Classes/StudentTest.cs
+++ 12.Classes/Classes/StudentTest.cs
@@ -8,30 +8,57 @@ namespace Classes
     {
         static void Main()
         {
+            
+            Console.Write("Enter your first name: ");
+            string firstName = Console.ReadLine();
 
-            Console.Write("Enter your three names: ");
-            string threeNames = Console.ReadLine();
+            Console.Write("Enter your last name: ");
+            string lastName = Console.ReadLine();
+
             Console.Write("Enter your year of studying: ");
             int year = int.Parse(Console.ReadLine());
+
             Console.Write("Enter your course: ");
             string course = Console.ReadLine();
+
             Console.Write("Enter the University you are currently studying in: ");
             string university = Console.ReadLine();
+
             Console.Write("Enter your E-mail address: ");
             string mail = Console.ReadLine();
+
             Console.Write("Enter your Phone Number: ");
             int phoneNumber = int.Parse(Console.ReadLine());
 
-            StudentProblem student1 = new StudentProblem(threeNames, year, course, university, mail, phoneNumber);
-            StudentProblem student2 = new StudentProblem(threeNames, year, course, university, mail, phoneNumber);
+            
 
+            StudentProblem student1 = new StudentProblem(firstName, lastName, year, course, university, mail, phoneNumber);
+            student1.FirstName = firstName;
+            student1.LastName = lastName;
+            student1.Year = year;
+            student1.Course = course;
+            student1.University = university;
+            student1.Email = mail;
+            student1.PhoneNumber = phoneNumber;
             student1.PrintStudentInfo();
+            
+            Console.WriteLine("First Name: " + student1.FirstName);
+            Console.WriteLine("Last Name: " + student1.LastName);
+            Console.WriteLine("Full Name: " + student1.FullName);
+            Console.WriteLine("Years of studying: " + student1.Year);
+            Console.WriteLine("Course: " + student1.Course);
+            Console.WriteLine("Uni: " + student1.University);
+            Console.WriteLine("Mail: " + student1.Email);
+            Console.WriteLine("Phone Number: " + student1.PhoneNumber);
+
+
+            StudentProblem student2 = new StudentProblem(firstName, lastName, year, course, university, mail, phoneNumber);
             student2.PrintStudentInfo();
 
             Console.WriteLine("The number of students is: " + student2.Counter);
 
             student2.Counter = 4;
-
+            
             Console.WriteLine("The number of students is: " + student2.Counter);
         }
     }
